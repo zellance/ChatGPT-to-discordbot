8853790557c65ba2b7a07452cad78a649876498e
zellance <NickNick1231234@gmail.com>
2023-09-03 12:52:20 +0300

    test

---
+++ config.py
@@ -0,0 +1,6 @@
+settings = {
+    'token': 'YOUR DISCORD TOKEN HERE',
+    'bot': 'Bot name here',
+    'id': 3123123,
+    'prefix': 'prefix here'
+}
---
+++ main.py
@@ -0,0 +1,36 @@
+import openai
+import discord
+from discord.ext import commands
+from config import settings
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=settings['prefix'], intents=intents)
+openai.api_key = "YOUR OPENAI KEY HERE"
+conversation_history = []
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    message_content = message.content
+    if message_content:
+        conversation_history.append({"role": "user", "content": message_content})
+
+    chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": "You're a virtual assistent."}] + conversation_history
+    )
+
+    reply = chat.choices[0].message.content
+    channel = message.channel
+    await channel.send(reply)
+
+
+bot.run('YOUR DISCORD TOKEN HERE')
